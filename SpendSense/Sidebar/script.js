182cf5867b617d1132e9d00435a5a74645457921
V-Paritosh <paritoshv08@gmail.com>
2025-02-04 23:17:08 -0600

    trying

--- SpendSense/Sidebar/script.js
+++ SpendSense/Sidebar/script.js
@@ -1,29 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const waitForSidebar = setInterval(() => {
-    const sidebar = document.getElementById("sidebar");
+const waitForSidebar = setInterval(() => {
+  const sidebar = document.getElementById("sidebar");
 
-    if (sidebar) {
-      clearInterval(waitForSidebar);
+  if (sidebar) {
+    clearInterval(waitForSidebar);
 
-      const currentPath = window.location.pathname;
-      console.log("Current Path:", currentPath);
+    const currentPath = window.location.pathname;
+    console.log("Current Path:", currentPath);
 
-      const navLinks = {
-        dashboard: "/SpendSense/Dashboard/index.html",
-        budget: "/SpendSense/Budget/index.html",
-        transactions: "/SpendSense/Transactions/index.html",
-      };
+    const navLinks = {
+      dashboard: "/SpendSense/Dashboard/index.html",
+      budget: "/SpendSense/Budget/index.html",
+      transactions: "/SpendSense/Transactions/index.html",
+    };
 
-      if (currentPath != "") {
-        for (const id in navLinks) {
-          if (currentPath === navLinks[id]) {
-            console.log(`Found active link: ${id}`);
-            document.getElementById(id).classList.add("active");
-          }
+    if (currentPath != "") {
+      for (const id in navLinks) {
+        if (currentPath === navLinks[id]) {
+          console.log(`Found active link: ${id}`);
+          document.getElementById(id).classList.add("active");
         }
-      } else {
-        document.getElementById("home").classList.add("active");
       }
+    } else {
+      document.getElementById("home").classList.add("active");
     }
-  }, 100);
-});
+  }
+}, 100);
