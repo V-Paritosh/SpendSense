ad5d940e5ee979c5ab3d552a72560071291c325b
V-Paritosh <paritoshv08@gmail.com>
2025-02-04 23:04:05 -0600

    Sidebar Done

---
+++ SpendSense/Sidebar/load.js
@@ -0,0 +1,12 @@
+function load() {
+  const x = new XMLHttpRequest();
+  x.open("GET", "/SpendSense/Sidebar/index.html", true);
+  x.onload = function () {
+    if (x.status === 200) {
+      document.getElementById("sidebar").innerHTML = x.responseText;
+      
+    }
+  };
+  x.send();
+}
+window.onload = load;
