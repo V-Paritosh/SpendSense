8479dc0ebafb203dc19a17bbe5de17bbf68b4f6b
V-Paritosh <paritoshv08@gmail.com>
2025-02-04 23:22:04 -0600

    fixed

--- SpendSense/Sidebar/load.js
+++ SpendSense/Sidebar/load.js
@@ -1,6 +1,11 @@
 function load() {
   const x = new XMLHttpRequest();
-  x.open("GET", "/SpendSense/Sidebar/index.html", true);
+  x.open(
+    "GET",
+    "https://v-paritosh.github.io/SpendSense/SpendSense/Sidebar/index.html",
+    true
+  );
+
   x.onload = function () {
     if (x.status === 200) {
       document.getElementById("sidebar").innerHTML = x.responseText;
